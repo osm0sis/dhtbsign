26785e4523fb9ac40d1a4aab4c58d000f3dca68e
osm0sis <osm0sis@outlook.com>
2017-09-06 18:17:22 -0300

    Rewrite as dhtbsign utility - check for existing DHTB magic and abort if present (i.e. already signed) - check for ANDROID! magic and abort if not present - check for SEANDROIDENFORCE and/or 0xFFFFFFFF payload padding and add if not present - make code formatting uniform - print useful information

--- dhtbsign.c
+++ dhtbsign.c
@@ -5,23 +5,22 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <limits.h>
-#include <arpa/inet.h>
 #include "mincrypt/sha256.h"
 
-const char T280_MAGIC[] =       {0x44, 0x48, 0x54, 0x42,0x01, 0x00, 0x00, 0x00};
-#define T280_HEADER_SZ          512
-#define T280_MAGIC_OFFSET       0
+const char DHTB_MAGIC[] =       {0x44, 0x48, 0x54, 0x42, 0x01, 0x00, 0x00, 0x00};
+#define DHTB_HEADER_SZ          512
 #define SHA256_OFFSET           8
 #define PAYLOAD_SZ_OFFSET       48
-#define SEANDROID_HEADER_SZ     680
-#define SEANDROID_MAGIC_SZ      20
-
-// Assumption is mkbootimg was used with --signature option to add on the required SEANDROID MAGIC (20 bytes) for sha256 calculation
-// Use "tools/seandroid_t280.img" or take a stock boot.img and run " tail -c 680 boot.img > seandroid_t280.img"
+#define BOOT_MAGIC              "ANDROID!"
+#define BOOT_MAGIC_SZ           8
+#define SEANDROID_MAGIC         "SEANDROIDENFORCE"
+#define SEANDROID_MAGIC_SZ      16
+const char PAYLOAD_PADDING[] =  {0xFF, 0xFF, 0xFF, 0xFF};
+#define TRAILING_PADDING_SZ     12
 
 int usage()
 {
-    printf("usage: mkT280bootimg\n");
+    printf("usage: dhtbsign\n");
     printf("\t-i|--input boot.img\n");
     printf("\t-o|--output boot_t280.img\n");
     return 0;
@@ -31,95 +30,135 @@ int main (int argc, char** argv)
 {
     char* iname = NULL;
     char* oname = NULL;
+    char tmp[4096];
 
     argc--;
     argv++;
-    while(argc > 0)
-    {
+    while(argc > 0) {
         char *arg = argv[0];
         char *val = argv[1];
         argc -= 2;
         argv += 2;
         if (!strcmp(arg, "--input") || !strcmp(arg, "-i"))
-        {
             iname = val;
-        }
         else if (!strcmp(arg, "--output") || !strcmp(arg, "-o"))
-        {
             oname = val;
-        }
         else
-        {
             return usage();
-        }
     }
 
-    if ((iname == NULL) || (oname == NULL)) {
+    if ((iname == NULL) || (oname == NULL))
         return usage();
-    }
 
     FILE *ifile = fopen(iname, "rb");
-    if( ifile == NULL )  {
+    if (ifile == NULL) {
         printf("Error opening input file %s!\n", iname);
         return usage();
     }
 
-    FILE *ofile = fopen(oname, "wb");
-    if( ofile == NULL )  {
-        printf("Error opening output file %s!\n", oname);
+    //Check for DHTB magic
+    fseek(ifile, 0, SEEK_SET);
+    if(fread(tmp, sizeof(DHTB_MAGIC), 1, ifile)){};
+    if (memcmp(tmp, DHTB_MAGIC, sizeof(DHTB_MAGIC)) == 0) {
+        printf("DHTB header magic already present!\n");
         fclose(ifile);
-        return usage();
+        return 1;
+    }
+
+    //Check for ANDROID! magic
+    fseek(ifile, 0, SEEK_SET);
+    if(fread(tmp, BOOT_MAGIC_SZ, 1, ifile)){};
+    if (memcmp(tmp, BOOT_MAGIC, BOOT_MAGIC_SZ) != 0) {
+        printf("Android boot magic not found!\n");
+        fclose(ifile);
+        return 1;
     }
 
     //Read boot.img filesize
     fseek(ifile, 0, SEEK_END);
     long isize = ftell(ifile);
-    fseek(ifile, 0, SEEK_SET);
-    if (isize <= SEANDROID_HEADER_SZ)
-    {
-        printf("Input file size <= SEANDROID Header for T280 (%d bytes): %ld\n", SEANDROID_HEADER_SZ, isize);
+
+    FILE *ofile = fopen(oname, "wb");
+    if (ofile == NULL) {
+        printf("Error opening output file %s!\n", oname);
         fclose(ifile);
-        fclose(ofile);
+        return usage();
     }
 
-    //Allocate header + boot.img size
-    unsigned char *bootimg = malloc(isize + T280_HEADER_SZ);
-    if (bootimg == NULL)
-    {
-        printf("Error allocating %ld bytes for output file!", (isize + T280_HEADER_SZ) );
+    //Check if SEANDROIDENFORCE footer and/or 0xFFFFFFFF padding are needed
+    printf("Input filesize: %d\n", isize);
+    int seeklimit = 65536;
+    int need_seandroid = 1;
+    int need_plpadding = 1;
+    for (int i = isize - seeklimit; i <= isize; i++) {
+        fseek(ifile, i, SEEK_SET);
+        if(fread(tmp, SEANDROID_MAGIC_SZ, 1, ifile)){};
+        if (memcmp(tmp, SEANDROID_MAGIC, SEANDROID_MAGIC_SZ) == 0) {
+            printf("SEAndroid footer at: %d\n", i);
+            need_seandroid = 0;
+            i = i + SEANDROID_MAGIC_SZ;
+            fseek(ifile, i, SEEK_SET);
+            if(fread(tmp, sizeof(PAYLOAD_PADDING), 1, ifile)){};
+            if (memcmp(tmp, PAYLOAD_PADDING, sizeof(PAYLOAD_PADDING)) == 0) {
+                printf("Payload padding at: %d\n", i);
+                need_plpadding = 0;
+                i = i + sizeof(PAYLOAD_PADDING);
+            }
+            isize = i;
+            break;
+        }
     }
-    fread( (bootimg + T280_HEADER_SZ), isize, 1, ifile);
+    printf("Input read size: %d\n", isize);
+
+    //Allocate header + boot.img size + trailing padding
+    unsigned char *bootimg = malloc(DHTB_HEADER_SZ + isize);
+    if (bootimg == NULL)
+        printf("Error allocating %ld bytes for output file!", (DHTB_HEADER_SZ + isize + 12));
+    fseek(ifile, 0, SEEK_SET);
+    if(fread((DHTB_HEADER_SZ + bootimg), isize, 1, ifile)){};
     fclose(ifile);
 
-    //Zero T280 HEADER
-    memset(bootimg, 0, T280_HEADER_SZ);
+    //Append SEANDROIDENFORCE footer and/or 0xFFFFFFFF padding if needed
+    if (need_seandroid == 1) {
+        printf("Adding SEANDROIDENFORCE footer...\n");
+        memcpy(bootimg + (DHTB_HEADER_SZ + isize), SEANDROID_MAGIC, SEANDROID_MAGIC_SZ);
+        isize = isize + SEANDROID_MAGIC_SZ;
+    }
+    if (need_plpadding == 1) {
+        printf("Adding payload padding...\n");
+        memcpy(bootimg + (DHTB_HEADER_SZ + isize), PAYLOAD_PADDING, sizeof(PAYLOAD_PADDING));
+        isize = isize + sizeof(PAYLOAD_PADDING);
+    }
 
-    //Calculate Payload Size
-    uint32_t psize = (isize - (SEANDROID_HEADER_SZ - SEANDROID_MAGIC_SZ));
+    //Zero DHTB header
+    memset(bootimg, 0, DHTB_HEADER_SZ);
+
+    //Calculate payload size
+    printf("Payload size: %d\n", isize);
+    uint32_t psize = isize;
 
     //Compute SHA256 of payload
     SHA256_CTX ctx256;
     SHA256_init(&ctx256);
-    SHA256_update(&ctx256, (bootimg + T280_HEADER_SZ), psize);
+    SHA256_update(&ctx256, (DHTB_HEADER_SZ + bootimg), psize);
     const uint8_t* sha256 = SHA256_final(&ctx256);
 
     //Copy MAGIC
-    memcpy((bootimg + T280_MAGIC_OFFSET), T280_MAGIC, sizeof(T280_MAGIC));
+    memcpy(bootimg, DHTB_MAGIC, sizeof(DHTB_MAGIC));
 
     //Copy SHA256
-    for(int i = 0; i < SHA256_DIGEST_SIZE; i++)
-    {
+    for (int i = 0; i < SHA256_DIGEST_SIZE; i++)
         bootimg[SHA256_OFFSET+i] = sha256[i];
-    }
 
-    //Copy Length
+    //Copy payload size as LE
     bootimg[PAYLOAD_SZ_OFFSET+0] = (psize >>  0) & 0xFF;
     bootimg[PAYLOAD_SZ_OFFSET+1] = (psize >>  8) & 0xFF;
     bootimg[PAYLOAD_SZ_OFFSET+2] = (psize >> 16) & 0xFF;
     bootimg[PAYLOAD_SZ_OFFSET+3] = (psize >> 24) & 0xFF;
 
-    //Write/Close/Free
-    fwrite(bootimg, (isize + T280_HEADER_SZ), 1, ofile);
+    //Write output file with additional padding, close/free
+    printf("Writing DHTB header + payload...\n");
+    fwrite(bootimg, (DHTB_HEADER_SZ + isize + TRAILING_PADDING_SZ), 1, ofile);
     fclose(ofile);
     free(bootimg);
  }
